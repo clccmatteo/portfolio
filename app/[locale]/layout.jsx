0a33ba75121c26bfeccabb8fc10bc8ec7f85ac29
Matteo <clccmatteo@gmail.com>
2024-12-27 19:12:38 +0100

    google analytics fix

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import Header from "../../components/header";
-import Script from "next/script";
 
 const Layout = async ({ children, params }) => {
   const resolvedParams = await params;
@@ -21,26 +20,22 @@ const Layout = async ({ children, params }) => {
         <meta name="author" content="Matteo Colucci" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/profile.png" />
-        {/* Google Analytics Script */}
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
       </head>
+      {/* Google Analytics Script */}
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `,
+        }}
+      />
       <body>
         <Header locale={locale} />
         <main className="pt-16 bg-gray-100">{children}</main>
